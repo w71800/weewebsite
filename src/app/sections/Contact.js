f8440d7463ed3230d03f2f71664a52b7e6666300
IKaRos <w71800@gmail.com>
2024-06-20 16:56:35 +0800

    feat: contact 區塊大致完成

--- src/app/sections/Contact.js
+++ src/app/sections/Contact.js
@@ -1,5 +1,17 @@
+import '@/styles/contact.sass'
+
 export default function Contact() {
   return (
-    <div id="contact"></div>
+    <div id="contact">
+      <div className="container">
+        <h1 className="title"> 有需求嗎？那就聯絡我們吧！ </h1>
+        <form action="">
+          <input type="text" name="name" placeholder="怎麼稱呼？"/>
+          <input type="text" name="contact" placeholder="聯絡方式" />
+          <textarea name="need" placeholder="需求說明"></textarea>
+          <input type="submit" value="送出" />
+        </form>
+      </div>
+    </div>
   )
 }
