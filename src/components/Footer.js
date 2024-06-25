7ebe19508c9a7d7065016b6d4b651cef58ea935e
IKaRos <w71800@gmail.com>
2024-06-25 15:03:02 +0800

    chore: 細修一下 footer 的樣式

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,39 +7,39 @@ export default function Footer({ data }) {
   return (
     <div id="footer">
       <div className="top">
-          <div className="container">
-            <div className="left">
-              <div className="column">
-                <div className="title">Wee Medical</div>
-                <div className="item">電話：04 23016388</div>
-                <div className="item">地址：403 台中市西區臺灣大道二段 186 號 20 樓之 1</div>
-                <div className="item">營業時間：週一～週五 10:00~18:30</div>
-              </div>
+        <div className="container">
+          <div className="column">
+            <div className="title">Wee Medical</div>
+            <div className="item">電話：04 23016388</div>
+            <div className="item">地址：403 台中市西區臺灣大道二段 186 號 20 樓之 1</div>
+            <div className="item">營業時間：週一～週五 10:00~18:30</div>
+          </div>
+            <div className="column">
+              <div className="title">產品列表</div>
+              {products.map( (product, index) => (
+                <div key={index} className="item"> {product} </div>
+              ))}
             </div>
-            <div className="right">
-              <div className="column">
-                <div className="title">產品列表</div>
-                {products.map( (product, index) => (
-                  <div key={index} className="item"> {product} </div>
-                ))}
-              </div>
-              
-              <div className="column">
-                <div className="title">服務列表</div>
-                {services.map( (service, index) => (
-                  <div key={index} className="item"> {service.title} </div>
-                ))}
-              </div>
-              <div className="column">
-                <div className="title">更多我們</div>
-                <div className="item">關於我們</div>
-                <div className="item">官方部落格</div>
+            
+            <div className="column">
+              <div className="title">服務列表</div>
+              {services.map( (service, index) => (
+                <div key={index} className="item"> {service.title} </div>
+              ))}
             </div>
-              </div>
+            <div className="column">
+              <div className="title">更多我們</div>
+              <div className="item">關於我們</div>
+              <div className="item">官方部落格</div>
+            </div>
           </div>
         </div>
         <div className="bottom">
-          <div className="container">2014 - 2024 為人有限公司 All Rights Reserved</div>
+          <div className="container">
+            2014 - 2024 為人有限公司 All Rights Reserved
+            <span className="privacy"> 隱私權政策 </span>
+            <span className="privacy"> COOKIE 政策 </span>
+          </div>
         </div>
     </div>
   )
